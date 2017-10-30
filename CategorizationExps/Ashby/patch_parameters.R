c39ba8c167e3d72c2cbddc00b2b5b0852604e68a
Kayleigh Ryherd <kryherd@d127h101.psy.uconn.edu>
2017-10-30 17:05:10 -0400

    Small fix

--- CategorizationExps/Ashby/patch_parameters.R
+++ CategorizationExps/Ashby/patch_parameters.R
@@ -2,26 +2,26 @@
 
 setwd("~/dissertation/CategorizationExps/Ashby")
 
-ii.freqa <- as.integer(rnorm(40, mean = 272, sd = 4538))
+ii.freqa <- as.integer(rnorm(5, mean = 272, sd = 4538))
 ii.freqa = .25*(ii.freqa/50)
-ii.ora <- as.integer(rnorm(40, mean = 153, sd = 4538))
+ii.ora <- as.integer(rnorm(5, mean = 153, sd = 4538))
 ii.ora <- ii.ora*(pi/500)
 
-rb.freqa <- as.integer(rnorm(40, mean = 260, sd = 75))
+rb.freqa <- as.integer(rnorm(5, mean = 260, sd = 75))
 rb.freqa = .25*(rb.freqa/50)
-rb.ora <- as.integer(rnorm(40, mean = 125, sd = 9000))
+rb.ora <- as.integer(rnorm(5, mean = 125, sd = 9000))
 rb.ora <- rb.ora*(pi/500)
 paramsa <- data.frame(cbind(ii.freqa, ii.ora, rb.freqa, rb.ora))
 
 
-ii.freqb <- as.integer(rnorm(40, mean = 327, sd = 4538))
+ii.freqb <- as.integer(rnorm(5, mean = 327, sd = 4538))
 ii.freqb = .25*(ii.freqb/50)
-ii.orb <- as.integer(rnorm(40, mean = 97, sd = 4538))
+ii.orb <- as.integer(rnorm(5, mean = 97, sd = 4538))
 ii.orb <- ii.orb*(pi/500)
 
-rb.freqb <- as.integer(rnorm(40, mean = 340, sd = 75))
+rb.freqb <- as.integer(rnorm(5, mean = 340, sd = 75))
 rb.freqb = .25*(rb.freqb/50)
-rb.orb <- as.integer(rnorm(40, mean = 125, sd = 9000))
+rb.orb <- as.integer(rnorm(5, mean = 125, sd = 9000))
 rb.orb <- rb.orb*(pi/500)
 paramsb <- data.frame(cbind(ii.freqb, ii.orb, rb.freqb, rb.orb))
 
