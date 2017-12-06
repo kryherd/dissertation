1d62516d56cc56a32e24d1cfcf4d01462678374f
Kayleigh Ryherd <kryherd@d127h101.psy.uconn.edu>
2017-12-06 14:18:28 -0500

    Updating psychopy

--- CatLearningfMRI/Experiment/blocktest.py
+++ CatLearningfMRI/Experiment/blocktest.py
@@ -10,10 +10,13 @@ import pandas as pd
 parent_dir = "./"
 
 #get some startup information from the user
-info = {'participant_id':'', 'order': ''}
-dlg = gui.DlgFromDict(info, title = 'Category Learning Startup')
+info = {'ID Number':'', 'Order': ''}
+dlg = gui.DlgFromDict(info, title='Category Learning Task')
 if not dlg.OK:
     core.quit()
+
+
+blockInstr = pd.read_table('blockInstr.csv', sep=',')
 
 #create dictionary for which type of block matches to which number
 block = {1: "UnsupervisedSparse", 2: "UnsupervisedDense", 3: "SupervisedSparse", 4: "SupervisedDense"}
@@ -22,8 +25,12 @@ order = {1: "Block1", 2: "Block2", 3: "Block3", 4: "Block4"}
 # read in whole order spreadsheet
 order_list = pd.read_table('blockOrders.csv', sep=',')
 # select the row that has the order entered in initially
-sel_order = order_list.loc[order_list['Order'] == int(info['order'])]
+sel_order = order_list.loc[order_list['Order'] == int(info['Order'])]
 # run through the blocks in ORDER!!!
 for i in range(1, 5):
     blockName = block[sel_order.iloc[0][order[i]]]
+    sel_instr = blockInstr.loc[blockInstr['BlockName'] == blockName, 'Instr']
+    block_instr = sel_instr.iloc[0][0:]
+
+
 
